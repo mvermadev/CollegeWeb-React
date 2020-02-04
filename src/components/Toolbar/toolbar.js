746f575cddc21011aaf9125b511ced3fa2c3ebc6
mvermadev <manishverma001997@gmail.com>
2020-02-04 20:02:04 +0530

    minor

--- src/components/Toolbar/toolbar.js
+++ src/components/Toolbar/toolbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from '../../pages/Registeration/Login';
 
 import DrawerToggleButton from '../SideDrawer/drawerToggleButton';
@@ -7,16 +7,18 @@ import './Toolbar.css';
 
 const toolbar = props => (
   <header className="toolbar">
-    <nav className="toolbar__navigation">
-        <div className="toolbar__toggle-button">
-            <DrawerToggleButton click={props.drawerClickHandler} />
-        </div>
-        <div className="toolbar__logo"><a href="/">E-services</a></div>
-        
-        <div className="toolbar_navigation-items">
-            <p><Link to={Login}>LOGIN</Link></p>
-        </div>
-    </nav>
+  <Router>
+  <nav className="toolbar__navigation">
+  <div className="toolbar__toggle-button">
+  <DrawerToggleButton click={props.drawerClickHandler} />
+  </div>
+  <div className="toolbar__logo"><a href="/">E-services</a></div>
+  
+  <div className="toolbar_navigation-items">
+  <p>LOGIN</p>
+  </div>
+  </nav>
+  </Router>
   </header>
 );
 
